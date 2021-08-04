968269a55891c4176bfeb822956d9a7e1a38885d
NothAmor <nothamor@NothAmordeMacBook-Pro.local>
2021-08-04 13:00:55 +0800

    Add random whole words function.

--- english.py
+++ english.py
@@ -69,33 +69,116 @@ words = {
             ["约翰看起来是个好人。即便如此，我还是不信任他。（even so）", "John seems (to be) a nice person. Even so, I don't trust him."]
         ],
         "lesson_12": [
-            ["如果他一开始谈论过去，你就永远都没法从他那儿脱身。（get away from）", "If he starts talking about the past, you'll never get away from him."]
+            ["如果他一开始谈论过去，你就永远都没法从他那儿脱身。（get away from）", "If he starts talking about the past, you'll never get away from him."],
+            ["冬天失业率有上升的趋势。（tendency）","There is a tendency for job losses to rise in the winter."],
+            ["在我不断地要求下，父亲终于同意和我一起去澳大利亚了。（frequent）", "Because of my frequent demands, father finally agreed to go to Australia with me."],
+            ["他把老店卖了，开了一家新店，以便赚更多的钱。（make money）", "He sold his shop and opened a new one to make more money."]
+        ],
+        "lesson_13": [
+            ["我们应该从失败中吸取教训, 这是很重要的。（learn a lesson from）", "It is important for us to learn a lesson from the failure."],
+            ["他相信自己想当证券经纪人的梦想总有一天会实现。（come true）", "He believes that one day his dream of becoming a stockbroker will come true."],
+            ["很多学生最后从事的工作不需要用到所学的知识。（end up）", "Many students end up doing jobs that do not make use of what they have learnt."],
+            ["我一提到他的名字，母亲就变得很不开心。（as soon as）", "As soon as I mentioned his name, my mother became very unhappy."]
+        ],
+        "lesson_14": [
+            ["只要你经常锻炼， 你又会变得健康起来。（as long as）", "As long as you get regular exercise, you will become healthy again."],
+            ["我一直想读一本有关太空的书，但是我好像总没时间去读。（get around to）", "I have always been thinking of reading a book on space, but I never seem to get around to it."],
+            ["那位作家自从买了电脑后，就再也不用笔写小说了。（no longer）", "Since the writer had bought the computer, he no longer wrote his stories with a pen."],
+            ["学校制定了一些新的规章制度，人人必须遵守。（set up）", "The school has set up some new rules that everybody must follow."],
+        ],
+        "lesson_15": [
+            ["看见大海，孩子们开心得大叫起来。（at the sight of）", "The children cried with delight at the sight of the sea."],
+            ["你刚刚说的话我没太听懂，你能再说一遍吗？（catch on）", "I didn't quite catch on to what you said just now. Would you say it again?"],
+            ["他知道那项任务很难，但还是接受了。（be conscious of）", "He was conscious of the difficulty of the task, but he still accepted it."],
+            ["直到现在，每当想起那天发生的事情时，我还是觉得莫名其妙。（to this day）", "To this day, when I recall what happened that day, I still feel confused."]
+        ],
+        "lesson_16": [
+            ["她似乎以贬低别人为快。（speak poorly of）", "She seems to enjoy speaking poorly of others."],
+            ["他过度地喝酒抽烟，结果死了。（as a consequence of）", "He died as a consequence of heavy drinking and smoking."],
+            ["你永远无法从他那儿得到直接的回答。（get... out of）", "You can never get a straight answer out of him."],
+            ["我们的产品在过去几年中逐渐受到欢迎。（little by little）", "Our products have become popular little by little over the past few years."]
+        ],
+        "lesson_17": [
+            ["我们明天就该开始进行那个项目了，可你却还没有准备好。（work on）", "We are supposed to start working on that project tomorrow, but you haven't got things ready."],
+            ["我今晚得把工作赶完，所以我不能和你一起去看电影了。（catch up on）", "I have to catch up on my work tonight, so I can't go to the movies with you."],
+            ["约翰不习惯这儿的新生活，所以打算搬走。（be accustomed to）", "John wants to move because he is not accustomed to the new life here."],
+            ["她伸手拿起电话，拨了一个朋友的号码。（reach for）", "She reached for her telephone and dialed the number of a friend."]
+        ],
+        "lesson_18": [
+            ["这个计划听起来虽然很难，但他决心将它付诸实施。（put something into practice）", "Difficult as the plan sounds, he is determined to put it into practice."],
+            ["面对这么多的困难，球队怎么能赢得比赛呢？（in the face of）", "How could the team win the game in the face of so many difficulties?"],
+            ["自两年前开业以来, 今年年初他的生意最兴隆。（at one's best）", "His business was at its best at the beginning of this year since it was started two years ago."],
+            ["他虽然没有直说，但我们能从他的手势中得到一些信息。（pick up）", "Although he didn't say it directly, we could pick up some messages from his gestures."]
+        ],
+        "lesson_19": [
+            ["我们必须想出解决这个问题的办法。（figure out）", "We must figure out how to solve the problem. OR: We must figure out the solutions to the problem."],
+            ["他看着包，像是在看着一件他看不懂的东西。（beyond one's comprehension）", "He looked at the bag as if he were looking at something beyond his comprehension."],
+            ["我会使用计算机，但是说到修计算机，我是一无所知。（come to）", "I know how to use a computer, but when it comes to repairing it, I know nothing about it."],
+            ["我们迷了路，更糟的是，天开始下雨了。（what's worse）", "We got lost. What's worse, it started to rain."]
+        ],
+        "lesson_20": [
+            ["学经验使她具有担任此项工作的条件。（qualify for）", "Her teaching experience qualifies her for the job."],
+            ["不适用于海外合资企业。（apply to）", "The new law does not apply to the joint venture overseas."],
+            ["如果你已决定租这所房子，请先付50美元。（in advance）", "If you have decided to rent this house, please pay 50 dollars in advance."],
+            ["一入境就被捕了。（upon/on entry to）", "Upon entry to the country, he was arrested."]
         ]
     }
 }
 
-while True:
-    lesson = input("请输入你想考核的单元.\n")
-    try:
-        lesson = int(lesson)
-        if lesson > 11:
-            print("数值超出支持范围, 支持(1-11)")
+print("请输入你想使用的功能的序号：")
+print("1.单元考核")
+print("2.全局随机抽考")
+print("请输入1或2")
+func_select = int(input())
+
+if func_select == 1:
+    while True:
+        lesson = input("请输入你想考核的单元.\n")
+        try:
+            lesson = int(lesson)
+            if lesson > 20:
+                print("数值超出支持范围, 支持(1-20)")
+                continue
+            else:
+                break
+        except Exception as e:
+            print("未知错误")
+
+    random_arr = []
+    while True:
+        if len(random_arr) == 4:
+            break
+        random_num = random.randint(0, 3)
+        if random_num in random_arr:
             continue
         else:
-            break
-    except Exception as e:
-        print("未知错误")
+            arr = words["words"]["lesson_{}".format(lesson)][random_num]
+            answer = input(arr[0] + "\n")
+            print("{}".format(arr[1]))
+            print(" ")
+            random_arr.append(random_num)
 
-random_arr = []
-while True:
-    if len(random_arr) == 4:
-        break
-    random_num = random.randint(0, 3)
-    if random_num in random_arr:
-        continue
-    else:
-        arr = words["words"]["lesson_{}".format(lesson)][random_num]
-        answer = input(arr[0] + "\n")
-        print("{}".format(arr[1]))
-        print(" ")
-        random_arr.append(random_num)
+elif func_select == 2:
+
+    words_exist = []
+    count = 0
+
+    print("请输入你想考核的个数:")
+    exam_num = int(input())
+
+    while True:
+        lesson_random = random.randint(1, 20)
+        words_random = random.randint(0, 3)
+
+        if count == 4:
+            break
+
+        if [lesson_random, words_random] in words_exist:
+            continue
+        else:
+            arr = words["words"]["lesson_{}".format(lesson_random)][words_random]
+            answer = input(arr[0] + "\n")
+            print("{}".format(arr[1]))
+            print(" ")
+            words_exist.append([lesson_random, words_random])
+            count += 1
